46bec8f3f2eafbc1f59bd9d8b01928b1eea8a7bf
Pritha <prithamondal004@gmail.com>
2023-08-17 21:11:08 +0530

    Project 2

---
+++ project2-contactus/src/components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,12 @@
+import styles from "./ContactHeader.module.css"
+
+const ContactHeader = () => {
+  return (
+    <div className={`${styles.contact_section} container`}>
+      <h1>CONTACT US</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate, dolor impedit reprehenderit, incidunt fugiat iste vel, tempora similique voluptas earum quam itaque voluptates corrupti eos rem soluta. Odit dolorum consequuntur eius non impedit voluptatum aut delectus voluptatibus tenetur, eum autem?</p>
+    </div>
+  )
+}
+
+export default ContactHeader
