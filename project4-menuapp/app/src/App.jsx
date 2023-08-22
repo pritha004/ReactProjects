a4a02febcc35033730a367c2845532a3339f5290
Pritha <prithamondal004@gmail.com>
2023-08-22 20:45:13 +0530

    Project 4

--- project4-menuapp/app/src/App.jsx
+++ project4-menuapp/app/src/App.jsx
@@ -1,5 +1,64 @@
+import styled from 'styled-components';
+
+
 const App = () => {
-  return <div>App</div>;
+  return(
+    <Container>
+      <TopContainer>
+          <div className="logo">
+            <img src="/logo.svg" alt="logo" />
+          </div>
+          <div className="search">
+            <input type="text" placeholder='Search Food...' />
+          </div>
+      </TopContainer>
+      <FilterContainer>
+        <Button>All</Button>
+        <Button>Breakfast</Button>
+        <Button>Lunch</Button>
+        <Button>Dinner</Button>
+      </FilterContainer>
+    </Container>
+  );
 };
 
 export default App;
+
+const Container=styled.div`
+max-width: 1200px;;
+margin: 0 auto;
+`;
+
+const TopContainer=styled.section`
+  min-height: 140px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+
+  .search{
+    input{
+      background: transparent;
+      border: 1px solid red;
+      border-radius: 5px;
+      color: white;
+      height: 40px;
+      font: 16px;
+      padding: 0 10px;
+    }
+  }
+`;
+
+const FilterContainer=styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+`;
+
+const Button=styled.button`
+  background-color:red;
+  color: white;
+  border-radius: 5px;
+  padding: 6px 12px;
+  border: none;
+`;
